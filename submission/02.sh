f6496b434e3299aaa12058103aa928cd81d8f6e3
Zairo <edwinmoyi8@email.com>
2026-03-30 11:37:42 +0300

    1-8 sol

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,7 @@
 # Create a native segwit address and get the public key from the address.
+
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" "bech32")
+# echo $ADDRESS
+
+PUBKEY=$(bitcoin-cli -regtest -rpcwallet=builderswallet getaddressinfo $ADDRESS | jq -r .pubkey)
+echo $PUBKEY
